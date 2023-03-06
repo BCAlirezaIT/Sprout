ab505296cccaa4a5ea3462fb1e1e47759031a89e
Isaiah Youm <isaiahyoum.coding@gmail.com>
2023-03-06 15:12:27 -0800

    Added necessary instance variables to user data.

--- public/js/authentication.js
+++ public/js/authentication.js
@@ -7,7 +7,9 @@ const uiConfig = {
       if (authResult.additionalUserInfo.isNewUser) {         //if new user
         db.collection("users").doc(user.uid).set({         //write to firestore. We are using the UID for the ID in users collection
           name: user.displayName,                    //"users" collection
-          email: user.email,                         //with authenticated user's ID (user.uid)
+          email: user.email,
+          friends: [],
+          country: undefined,                  //with authenticated user's ID (user.uid)
         }).then(function () {
           console.log("New user added to firestore");
           window.location.assign("main.html");       //re-direct to main.html after signup
